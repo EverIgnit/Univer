f2a45f5567e9c85a7c02f7bbfb1d3b3bfc85bf29
EverIgnit <gbelkevich@bk.ru>
2021-05-21 15:47:35 +0300

    Added comments

--- Sem6V1/MAI/Lab4/Lab4/Program.cs
+++ Sem6V1/MAI/Lab4/Lab4/Program.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using Var1;
 using System.Linq;
 using System.Collections.Generic;
 using System.Collections;
 class Program
 {
+    // Fields to display info about
     public AirPort shumilino = new();
     public List<AirPort> moscowAirPorts = new();
-    static void Main() => new Program().F();
-    void F()
+    // Program starting point
+    static void Main() => new Program().MainFunc();
+    // Main program
+    void MainFunc()
     {
+        // Creating objects to display
         PlaneRace planeRace = new() { Arrival = DateTime.Now, Departure = DateTime.Now, Sits = new Random().Next(50, 200) };
         Ticket ticket = new() { Price = new Random().Next(100, 1000) };
         planeRace.Tickets.Add(ticket);
@@ -16,15 +21,14 @@ class Program
         shumilino.PlaneRacesTimeTable.Add(planeRace);
         moscowAirPorts.Add(shumilino);
         moscowAirPorts.Add(shumilino);
+        // Hadnling the input
         Console.WriteLine("Select a variable by its number:");
         typeof(Program).GetFields().ToList().ForEach(Console.WriteLine);
         var choise = typeof(Program).GetFields()[int.Parse(Console.ReadLine())];
-        Console.WriteLine($"{choise.Name}{InfoDisplayer.InfoBy(choise.GetValue(this))}");
+        Console.WriteLine($"{choise.Name}{InfoOf(choise.GetValue(this))}");
     }
-}
-static class InfoDisplayer
-{
-    public static string InfoBy(object obj)
+    // Method return the info of the specified object and it's parts
+    static string InfoOf(object obj)
     {
         var type = obj.GetType();
         var res = $" - a{("eyuioa".ToUpper().Contains(type.Name.First()) ? 'n' : string.Empty)} {type.Name}";
@@ -35,7 +39,7 @@ static class InfoDisplayer
             var count = 0;
             foreach (var el in list)
             {
-                res += $"\n{count}{InfoBy(el)}";
+                res += $"\n{count}{InfoOf(el)}";
                 count++;
             }
         }
@@ -44,38 +48,43 @@ static class InfoDisplayer
             res += " which has";
             var props = type.GetProperties();
             foreach (var prop in props)
-                res += $"\n{prop.Name}{InfoBy(prop.GetValue(obj))}";
+                res += $"\n{prop.Name}{InfoOf(prop.GetValue(obj))}";
         }
         else
             res += " and it equals to " + obj;
         return res.Replace("\n", "\n ");
     }
 }
-class AirPort 
+// Change the classes of this namespace to change your variant
+namespace Var1
 {
-    public TimeTable PlaneRacesTimeTable { get; } = new();
-    public TicketWindow TicketWindow { get; } = new();
-}
-class TimeTable : List<PlaneRace>{}
-class PlaneRace 
-{
-    public int Sits { get; init; }
-    public DateTime Arrival { get; set; }
-    public DateTime Departure { get; set; }
-    public List<Ticket> Tickets { get; } = new();
-}
-class TicketWindow 
-{
-    public List<Ticket> Tickets { get; } = new();
-    public bool IsAvaliable(int num) => Tickets.ElementAtOrDefault(num)?.Avaliable ?? false;
-    public void BuyTicket(int num) { if (IsAvaliable(num)) Tickets.RemoveAt(num); }
-}
-class Ticket 
-{
-    public virtual int Price { get; set; }
-    public bool Avaliable { get; set; } = true;
-}
-class LuxTicket : Ticket
-{
-    public override int Price { get => base.Price * 2; set => base.Price = value; }
+    class AirPort
+    {
+        public TimeTable PlaneRacesTimeTable { get; } = new();
+        public TicketWindow TicketWindow { get; } = new();
+    }
+    class TimeTable : List<PlaneRace> { }
+    class PlaneRace
+    {
+        public int Sits { get; init; }
+        public DateTime Arrival { get; set; }
+        public DateTime Departure { get; set; }
+        public List<Ticket> Tickets { get; } = new();
+    }
+    class TicketWindow
+    {
+        public List<Ticket> Tickets { get; } = new();
+        public bool IsAvaliable(int num) => Tickets.ElementAtOrDefault(num)?.Avaliable ?? false;
+        public void BuyTicket(int num) { if (IsAvaliable(num)) Tickets.RemoveAt(num); }
+    }
+    class Ticket
+    {
+        public virtual int Price { get; set; }
+        public bool Avaliable { get; set; } = true;
+    }
+    class LuxTicket : Ticket
+    {
+        public override int Price { get => base.Price * 2; set => base.Price = value; }
+    }
+
 }
